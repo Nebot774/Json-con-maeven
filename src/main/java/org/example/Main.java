6395d9bf489f19f31e7010306033d90a991b3f4f
Nebot774 <adriclemenbot@gmail.com>
2023-10-25 18:49:17 +0200

    proyecto echo con maeven para las dependencias de json 1. Define una clase Book para representar un libro con los atributos mencionados anteriormente.                                                        2. Crea una lista de libros (puedes utilizar una lista de objetos Book) y agrégale algunos libros de                                                        ejemplo.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //instanciamos array de objetos book
+        List<Book> libros=new ArrayList<>();
+
+        //creamos objetos book
+        Book libro1 = new Book("978-1-234567-89-0", "El Gran Gatsby", "F. Scott Fitzgerald", 180, 1925);
+        Book libro2 = new Book("978-2-345678-90-1", "Cien años de soledad", "Gabriel García Márquez", 368, 1967);
+        Book libro3 = new Book("978-3-456789-01-2", "1984", "George Orwell", 328, 1949);
+
+        //añadimos l,os objetos al array
+        libros.add(libro1);
+        libros.add(libro2);
+        libros.add(libro3);
+
+
+    }
+    //Utiliza la librería Jackson para serializar la lista de libros a un archivo JSON
+
+    public static String escibriListaObjetosaJson(List<Book> libros){
+        try{
+            ObjectMapper objectMapper=new ObjectMapper();
+
+
+        }
+
+    }
+
+
+}
